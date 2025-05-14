29cdc0b14fc52236dfaf926085f7922a15008f8c
FrankFeitosa <feitosfrank22@gmail.com>
2025-05-14 17:59:51 -0300

    feat: create two files and dir userController.js and userRoutes.js

--- src/app.js
+++ src/app.js
@@ -1,5 +1,8 @@
 import express from 'express';
+import userRoutes from '../src/routes/userRoutes.js'
 
 const app = express();
+
+app.use('/user', userRoutes)
 
 export default app;
---
+++ src/controllers/userController.js
@@ -0,0 +1,11 @@
+import {PrismaClient} from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+export const getAllUsers = async (req,res) => {
+    try {
+        
+    } catch (error) {
+        
+    }
+}
