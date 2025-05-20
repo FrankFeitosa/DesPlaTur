c2ab42cb812aa3d0c77af06bed950e2ab92b536b
FrankFeitosa <feitosfrank22@gmail.com>
2025-05-20 14:17:04 -0300

    feat: Create utils/auth.js

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,15 +1,14 @@
 import express from 'express';
-import { createAllUsers, deleteUsers, getAllUsers, getIdUser, updateAllUsers } from '../controllers/userController.js';
+import {  loginUser, registerUser } from '../controllers/userController.js';
 import { validate } from '../middleware/validate.js';
-import { createUserSchema } from '../schemas/userSchemas.js';
+import { loginUserRegisterSchema, registerUserSchema } from '../schemas/userSchemas.js';
 
 const router = express.Router();
 
-router.get('/', getAllUsers);
-router.get('/:id', getIdUser);
-router.post('/auth/register',validate(createUserSchema), createAllUsers);
-router.put('/:id', updateAllUsers);
-router.delete('/:id', deleteUsers);
+
+router.post('/auth/register',validate(registerUserSchema), registerUser);
+router.post('/auth/login',validate(loginUserRegisterSchema), loginUser);
+
 
 
 export default router
---
+++ src/utils/auth.js
@@ -0,0 +1,25 @@
+import bcrypt from 'bcrypt'; 
+import jwt from 'jsonwebtoken';
+
+const saltRounds = 10
+const JWT_SECRET = process.env.JWT_SECRET
+
+export async function hashPassword(password){
+    return await bcrypt.hash(password, saltRounds)
+}
+
+export async function compare(password,hashPassword) {
+    return await bcrypt.compare(password, hashPassword)
+}
+
+export function generateToken(user) {
+    return jwt.sign(
+        {id: user.id, email: user.email},
+        JWT_SECRET,
+        {expiresIn: '1h'}
+    )
+}
+
+export function verifyToken(token) {
+    return jwt.verify(token, JWT_SECRET)
+}
